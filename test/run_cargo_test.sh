7116162c121adbfc19c0bd7613eec9d4f225cdbe
Scott Hain <shain@chef.io>
2019-03-27 13:49:16 -0700

    use bootstrap package

--- test/run_cargo_test.sh
+++ test/run_cargo_test.sh
@@ -32,35 +32,36 @@ cargo_test_command="cargo test ${features_string} -- --nocapture ${test_options:
 # . ./test/_osx_test_setup.sh
 
 export SSL_CERT_FILE=/usr/local/etc/openssl/cert.pem
+wget https://s3-us-west-2.amazonaws.com/shain-bk-test/mac-bootstrapper-1.0.0-latest.pkg
+sudo installer -pkg mac-bootstrapper-1.0.0-latest.pkg -target /
 
-sudo hab pkg install core/rust
-
+# sudo hab pkg install core/rust
 
 # TODO: these should be in a shared script?
-sudo hab pkg install core/bzip2
-sudo hab pkg install core/libarchive
-sudo hab pkg install core/libsodium
-sudo hab pkg install core/openssl
-sudo hab pkg install core/xz
-sudo hab pkg install core/zeromq
-sudo hab pkg install core/protobuf --binlink
+# sudo hab pkg install core/bzip2
+# sudo hab pkg install core/libarchive
+# sudo hab pkg install core/libsodium
+# sudo hab pkg install core/openssl
+# sudo hab pkg install core/xz
+# sudo hab pkg install core/zeromq
+# sudo hab pkg install core/protobuf --binlink
 
 export SODIUM_STATIC=true # so the libarchive crate links to sodium statically
 export LIBARCHIVE_STATIC=true # so the libarchive crate *builds* statically
 export OPENSSL_DIR # so the openssl crate knows what to build against
-OPENSSL_DIR="$(hab pkg path core/openssl)"
+OPENSSL_DIR=/opt/mac-bootstrapper/embedded/lib
 export OPENSSL_STATIC=true # so the openssl crate builds statically
 export LIBZMQ_PREFIX
-LIBZMQ_PREFIX=$(hab pkg path core/zeromq)
+LIBZMQ_PREFIX=/opt/mac-bootstrapper/embedded/lib
 # now include openssl and zeromq so thney exists in the runtime library path when cargo test is run
 export LD_LIBRARY_PATH
-LD_LIBRARY_PATH="$(hab pkg path core/libsodium)/lib:$(hab pkg path core/zeromq)/lib"
+LD_LIBRARY_PATH=/opt/mac-bootstrapper/embedded/lib
 # include these so that the cargo tests can bind to libarchive (which dynamically binds to xz, bzip, etc), openssl, and sodium at *runtime*
 export LIBRARY_PATH
-LIBRARY_PATH="$(hab pkg path core/bzip2)/lib:$(hab pkg path core/libsodium)/lib:$(hab pkg path core/openssl)/lib:$(hab pkg path core/xz)/lib"
+LIBRARY_PATH=/opt/mac-bootstrapper/embedded/lib
 # setup pkgconfig so the libarchive crate can use pkg-config to fine bzip2 and xz at *build* time
 export PKG_CONFIG_PATH
-PKG_CONFIG_PATH="$(hab pkg path core/libarchive)/lib/pkgconfig:$(hab pkg path core/libsodium)/lib/pkgconfig:$(hab pkg path core/openssl)/lib/pkgconfig"
+PKG_CONFIG_PATH=/opt/mac-bootstrapper/embedded/lib/pkgconfig
 
 # Set testing filesystem root
 export TESTING_FS_ROOT
