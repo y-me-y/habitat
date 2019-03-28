13cf1e4c55d0b75ab0e5f028f9aaeecc79bf33bf
Scott Hain <shain@chef.io>
2019-03-28 10:15:40 -0700

    too many dashes;

--- test/run_cargo_test.sh
+++ test/run_cargo_test.sh
@@ -35,7 +35,7 @@ export SSL_CERT_FILE=/usr/local/etc/openssl/cert.pem
 curl https://s3-us-west-2.amazonaws.com/shain-bk-test/mac-bootstrapper-1.0.0-latest.pkg -o mac-bootstrapper-1.0.0-latest.pkg
 sudo installer -pkg mac-bootstrapper-1.0.0-latest.pkg -target /
 
-curl https://sh.rustup.rs -sSf | sh - -y
+curl https://sh.rustup.rs -sSf | sh -- -y
 rustup install stable
 rustup default stable
 
