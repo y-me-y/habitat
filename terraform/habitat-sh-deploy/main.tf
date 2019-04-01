f7d943652f4c6a34dd4b5fa5163f8d4c3bcc18af
Scott Hain <shain@chef.io>
2019-04-01 14:04:30 -0700

    blarrrggg

---
+++ terraform/habitat-sh-deploy/main.tf
@@ -0,0 +1,33 @@
+module "cd_infrastructure" {
+  source      = "git@github.com:chef/es-terraform.git//modules/cd_common_infrastructure"
+  environment = "delivered"
+}
+
+provider "aws" {
+  region  = "${module.cd_infrastructure.aws_region}"
+  profile = "${module.cd_infrastructure.aws_profile}"
+}
+
+# data "external" "latest_hab_pkg" {
+#   program = ["bash", "-c", "${path.module}/scripts/latest-hab-pkg.sh"]
+
+#   query = {
+#     channel = "${var.channel}"
+#   }
+# }
+
+module "habitat_sh_site" {
+  source    = "git@github.com:chef/es-terraform.git//modules/cd_hugo_static_site"
+  subdomain = "habitat-sh-${var.dns_suffix}"
+
+  site_dir     = "./build"
+  fastly_fqdn = "${var.fastly_fqdn}"
+
+  # build_command = "BUILDER_WEB_URL='https://bldr.acceptance.habitat.sh' GITHUB_APP_URL='https://github.com/apps/habitat-builder-acceptance' make build"
+
+  build_command = "pwd"
+
+  # AWS Tags
+  tag_dept    = "CoreEng"
+  tag_contact = "releng"
+}
