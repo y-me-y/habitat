90b0a0667c84c60e077f22a21f83139e4d25169e
Scott Hain <shain@chef.io>
2019-04-02 15:00:48 -0700

    test more

--- terraform/habitat-sh-deploy/main.tf
+++ terraform/habitat-sh-deploy/main.tf
@@ -8,50 +8,8 @@ provider "aws" {
   profile = "${module.cd_infrastructure.aws_profile}"
 }
 
-# data "external" "latest_hab_pkg" {
-#   program = ["bash", "-c", "${path.module}/scripts/latest-hab-pkg.sh"]
-
-#   query = {
-#     channel = "${var.channel}"
-#   }
-# }
-
-# resource "null_resource" "build_habitat_sh" {
-#   triggers = {
-#     always_do = "${uuid()}"
-#   }
-
-  # provisioner "local-exec" {
-  #   command = "make build"
-  #   working_dir = "../../www"
-  # }
-
-  # provisioner "local-exec" {
-  #   command = "make deploy"
-  #   working_dir = "../../www"
-  # }
-
-  # provisioner "local-exec" {
-  #   command = "chmod +x /tmp/chef-automate"
-  # }
-
-  # provisioner "local-exec" {
-  #   command = "/tmp/chef-automate airgap bundle create /tmp/automate.aib --channel ${var.channel}"
-  # }
-# }
-
-# module "habitat_sh_site" {
-#   source    = "git@github.com:chef/es-terraform.git//modules/cd_s3_website"
-#   subdomain = "habitat-sh-${var.dns_suffix}"
-#   create = "true"
-#   # create    = "${var.environment == "delivered" ? "true" : "false"}"
-# }
-
-
-
 module "habitat_sh_site" {
-  # source    = "git@github.com:chef/es-terraform.git//modules/cd_hugo_static_site"
-  source    = "/Users/shain/code/chef/es-terraform/modules/cd_generic_static_site"
+  source    = "git@github.com:chef/es-terraform.git//modules/cd_generic_static_site"
   subdomain = "habitat-sh-${var.dns_suffix}"
 
   site_dir      = "../../www"
