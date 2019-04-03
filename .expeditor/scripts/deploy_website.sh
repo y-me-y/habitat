8dca1773679aef9895d944f51b80971c869014f7
Scott Hain <shain@chef.io>
2019-04-03 12:52:00 -0700

    hardcode acceptance

--- .expeditor/scripts/deploy_website.sh
+++ .expeditor/scripts/deploy_website.sh
@@ -5,16 +5,19 @@ set -euo pipefail
 # This block translates the "channel" into the appropriate set of VPC settings
 # used in terraform/Makefile. These settings still rely on old Workflow-isms,
 # which is why the TF_ENVIRONMENT name doesn't match up with the CHANNEL.
-if [ "$CHANNEL" == "unstable" ]; then
-  export TF_ENVIRONMENT=acceptance
-elif [ "$CHANNEL" == "dev" ]; then
-  export TF_ENVIRONMENT=union
-elif [ "$CHANNEL" == "acceptance" ]; then
-  export TF_ENVIRONMENT=delivered
-else
-  puts "We do not currently support deploying channel $CHANNEL"
-  exit 1
-fi
+# if [ "$CHANNEL" == "unstable" ]; then
+#   export TF_ENVIRONMENT=acceptance
+# elif [ "$CHANNEL" == "dev" ]; then
+#   export TF_ENVIRONMENT=union
+# elif [ "$CHANNEL" == "acceptance" ]; then
+#   export TF_ENVIRONMENT=delivered
+# else
+#   puts "We do not currently support deploying channel $CHANNEL"
+#   exit 1
+# fi
+
+# Hardcode to acceptance for now.
+export TF_ENVIRONMENT=acceptance
 
 # export LOGS_URL="$BUILDKITE_BUILD_URL"
 
