9c654dc1bf9c354f19a2404bbb268d80170e0402
Scott Hain <shain@chef.io>
2019-07-31 12:28:44 -0700

    TEST BINS
    Signed-off-by: Scott Hain <shain@chef.io>

--- .expeditor/scripts/build_component.sh
+++ .expeditor/scripts/build_component.sh
@@ -31,6 +31,29 @@ echo "--- :habicat: Building components/${component}"
 # This is a temporary measure so we can run fake releases
 export HAB_STUDIO_SECRET_DO_FAKE_RELEASE=$DO_FAKE_RELEASE
 
+env HAB_STUDIO_ROOT=/hab/studio/inspectorama HAB_BLDR_CHANNEL="${channel}" ${hab_binary} studio new
+
+echo "--- :habicat: Inspecting studio"
+echo "/bin/sh"
+file /hab/studios/inspectorama/bin/sh
+if file -bi /hab/studios/inspectorama/bin/sh == "text/x-shellscript; charset=us-ascii"; then
+  cat /hab/studios/inspectorama/bin/sh
+fi
+
+echo "/bin/bash"
+file /hab/studios/inspectorama/bin/bash
+if file -bi /hab/studios/inspectorama/bin/sh == "text/x-shellscript; charset=us-ascii"; then
+  cat /hab/studios/inspectorama/bin/bash
+fi
+
+echo "/bin/hab"
+file /hab/studios/inspectorama/bin/hab
+if file -bi /hab/studios/inspectorama/bin/sh == "text/x-shellscript; charset=us-ascii"; then
+  cat /hab/studios/inspectorama/bin/hab
+fi
+
+exit 1
+
 HAB_BLDR_CHANNEL="${channel}" ${hab_binary} pkg build "components/${component}"
 source results/last_build.env
 
